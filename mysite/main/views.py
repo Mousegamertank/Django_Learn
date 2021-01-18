4f9f8caf4543dafe893f6dd006bddd4e4b41a0ec
Pedro.Marçal <62404639+Mousegamertank@users.noreply.github.com>
2021-01-18 17:31:57 -0300

    day 01, learning how to work with views and Urls

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index (response):
-    pass
+    return HttpResponse('<h1>Pedro with pedro!</h1>')
+
+def v1(response):
+    return HttpResponse('<h1> V1 <h1>')
