86bc143af65f53ea87323a8741e31d35155a4783
Pedro.Marçal <62404639+Mousegamertank@users.noreply.github.com>
2021-01-20 17:33:57 -0300

    I_learned_how_to_use_Django_with_database_and_how_to_use_admin_painel_in_the_end_from_day_3

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>', views.showName, name='index'),
+    path('<str:name>', views.showNameDire, name='index'),
     path('v1/', views.v1, name='index'),
+   # path('<int:id>', views.showId, name='index'),
 ]
 
 
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
 def index (response):
-    return HttpResponse('<h1>Pedro with pedro!</h1>')
+    return HttpResponse('<h1>Pedro List</h1>' )
+
+#def showId (response, id):
+ #   return HttpResponse('<h1>{}</h1>'.format(id))
+
+def showName(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse('<h1>{}</h1>'.format(ls.name))
+
+def showNameDire(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse('<h1>{}</h1><br></br><p>{}</p>'.format(ls.name, str(item.text)))
 
 def v1(response):
-    return HttpResponse('<h1> View 1 <h1>')
+    return HttpResponse('<h1> Deus Viniscius comedor de todas </h1>')
